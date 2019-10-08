c262c41341f4f0910e2a824fe1dfb256b49efc3b
github username <github email address>
2019-10-08 14:00:19 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+    def furry
+        puts "What a cute dog!"
+    end 
+end
+
+fido = Dog.new
+fido.furry 
+
+snoopy = Dog.new
+snoopy.furry
+
+lassie = Dog.new
+lassie.furry
+
+class Person
+    def nice
+        puts "What a great person!"
+    end
+end 
+
+
+
+
+
